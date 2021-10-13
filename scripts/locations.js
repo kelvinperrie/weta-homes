c53b65f8fde29b7a72c36b8d20949c74f6b3c3a7
kelvin <kelvinperrie@gmail.com>
2021-10-13 14:56:59 +1300

    updating location info and missing pictures and instruction content

--- scripts/locations.js
+++ scripts/locations.js
@@ -1,42 +1,42 @@
 var houseLocations = [
     {
         title: "Matekai Park 1",
-        description: "The house is on a large tree about 10 meters behind the left side of the bench. It is on the side of the tree facing away from the track/bench.",
+        description: "The house is on a large tree about 10 meters behind the left side of the bench. It is on the side of the tree facing away from the track/bench. It can be tricky to see, find, and get too.",
         latLong: [173.950859, -39.115783]
     },
     {
         title: "Matekai Park 2",
-        description: "Off track",
+        description: "This house is about 3 or 4 meters off the track. You have to push through a few branches and up a little step. It's on a tree facing towards the houses/ocean.",
         latLong: [173.952160,-39.116347]
     },
     {
         title: "Matekai Park 3",
-        description: "Go past the tire swing towards the stream, the house is about half way between the track and the stream on a large tree.",
+        description: "Go past the tire swing towards the stream, the house is about half way between the track and the stream on a large tree. You might not see it walking towards the stream, it's easier to spot when walking back towards the tire swing/track.",
         latLong: [173.952554,-39.117072]
     },
     {
         title: "Corbet Park 1",
-        description: "At top at road end",
+        description: "This is on the path that goes up the hill behind Corbet Park. It's at the top of the hill, at the ocean end. It's on the farm side of the track.",
         latLong: [173.958615, -39.109225]
     },
     {
         title: "Water Front 1",
-        description: "Close to campground",
+        description: "Take the path behind the trees. This is at the camp ground end of the path. It is about 6 to 10 meters off the track away from the ocean and up the hill and is on a tree facing down the coast away from the camp ground. It is not always visible from the track (depends where you're standing).",
         latLong: [173.938691, -39.118024]
     },
     {
         title: "Water Front 2",
-        description: "Close to waterfall",
+        description: "This is on the path behind the trees. This is at the waterfall end of the path. It is a couple of meters off the track on the ocean side. It's visible from the track.",
         latLong: [173.937393, -39.118098]
     },
     {
         title: "Behind playground 1",
-        description: "Not far past the skate ramp",
+        description: "This is on the track, not far past the skate ramp. It's not on the stream side of the track. It's easily visible from the track.",
         latLong: [173.948385, -39.116783]
     },
     {
         title: "Behind playground 2",
-        description: "On the path from the playground toward Telford Terrace",
+        description: "On the path from the Shearer Reserve playground towards Telford Terrace. It's around 1 meter off the track, on the side away from Wairau road. It's visible from the track but might be behind some branches and leaves.",
         latLong: [173.949323, -39.117986]
     }
 ];
